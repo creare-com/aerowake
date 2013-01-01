eb90f1e023bd861484455645e60f28a28303623f
Brandon Draper <draper@mit.edu>
2012-12-31 19:27:16 -0500

    Switched back to symbolic links for pixhawk and radio connection.

--- Flight source/uav/main.py
+++ Flight source/uav/main.py
@@ -274,12 +274,12 @@ if __name__ == '__main__':
 
 	# uav_connect_path = '/dev/ttyACM0' # Use for odroid through pixhawk usb cord
 	# uav_connect_path = '/dev/ttyUSB0' # Use for odroid through usb to serial converter
-	uav_connect_path = '/dev/ttySAC0' # Use for odroid through GPIO pins
-	# uav_connect_path = '/dev/pixhawk' # Use after configuring symbolic link through udevadm
+	# uav_connect_path = '/dev/ttySAC0' # Use for odroid through GPIO pins
+	uav_connect_path = '/dev/pixhawk' # Use after configuring symbolic link through udevadm
 	uav_baud = 57600
 
-	gcs_connect_path = '/dev/ttyUSB0' # Use for telemetry radio through usb port
-	# gcs_connect_path = '/dev/radioacl33' # Use after configuring symbolic link through udevadm
+	# gcs_connect_path = '/dev/ttyUSB0' # Use for telemetry radio through usb port
+	gcs_connect_path = '/dev/radioacl33' # Use after configuring symbolic link through udevadm
 	gcs_baud = 57600
 
 	#-----------------------------------------------------------------------------
