beacea8864ead3a929e6ada9ed74d4ddac0320e5
John Walthour <jdw@creare.com>
2016-01-08 09:21:07 -0500

    Bugfixes

--- Logging/I2cPressureSensorLogger.py
+++ Logging/I2cPressureSensorLogger.py
@@ -31,7 +31,7 @@ class PressureSensor(object):
         busnum = I2C.get_default_bus()
         self._bus = smbus.SMBus(busnum)
     
-    def get_desc():
+    def get_desc(self):
         return self.description
     
     def read_p(self):
@@ -80,27 +80,26 @@ if __name__ == "__main__":
     import time
     
     # Settings
-
     FSP_lo = 10 #inH20, for DLVR-F50D-E2NS-C-NI3F
     FSP_hi = 1  #inH20, for DLVR-L05D-E3NS-C-NI3F
     # Descriptions will be used for column names, so the units are listed here too
     probe_sensor_settings = [
-        {'desc':'Ch0  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch0  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch1  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch1  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch2  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch2  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch3  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch3  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch4  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch4  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch5  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch5  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch6  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch6  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch7  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch7  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch8  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch8  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch9  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch9  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch10 lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch10 hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
-        {'desc':'Ch11 lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {desc:'Ch11 hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch0  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch0  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch1  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch1  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch2  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch2  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch3  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch3  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch4  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch4  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch5  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch5  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch6  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch6  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch7  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch7  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch8  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch8  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch9  lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch9  hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch10 lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch10 hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
+        {'desc':'Ch11 lo (inH2O)', 'addr':0x18, 'FSP':FSP_lo}, {'desc':'Ch11 hi (inH2O)', 'addr':0x20, 'FSP':FSP_hi}, 
     ]
-    probe_sensors = [DlvrPressureSensor(**set) for set in settings]
-    absolute_sensor = DlvPressureSensor(addr=0x18, 
-    csv_column_names=['System time (s)', 'Time since previous line (ms)'] + [sensor.get_desc() for sensor in probe_sensors]
+    probe_sensors = [DlvrPressureSensor(**set) for set in probe_sensor_settings]
+    absolute_sensor = DlvPressureSensor(desc='Absolute pressure (PSIA)', addr=0x18, FSP=30)
+    csv_column_names=['System time (s)', 'Time since previous line (ms)'] + [sensor.get_desc() for sensor in probe_sensors] + [absolute_sensor.get_desc()]
     logfile_name = 'pressure_log.csv'
     temp_read_interval_s = 1.0 # Read temperature and absolute pressure every second
     
@@ -112,24 +111,20 @@ if __name__ == "__main__":
             now = time.time()
             dt = 0
             while True:
-                pressures = [] # apparently list append is fairly fast
-                for sensor in diff_p_sensors:
+                probe_pressures = [] # apparently list append is fairly fast
+                for sensor in probe_sensors:
                     p = sensor.read_p()
-                    # p,t = sensor.read_pt()
-                    pressures.append(p)
-                pressure_str=','.join([str(p) for p in pressures])
+                    probe_pressures.append(p)
+                probe_pressure_str=','.join([str(p) for p in probe_pressures])
                 if now - time_last_read_temp >= temp_read_interval_s:
-                    #TODO: read these sensors and log them
-                    # print "Reading and logging temperature and absolute pressure. %.3f"%(dt * 1000)
                     t = 0
-                    ap = 0
+                    ap_str = str(absolute_sensor.read_p())
                     time_last_read_temp = now
+                    log_str = '%.3f,%.3f,%s,%s\n'%(now,dt*1000,probe_pressure_str,ap_str)
                 else:
-                    t = None
-                    ap = None
+                    log_str = '%.3f,%.3f,%s\n'%(now,dt*1000,probe_pressure_str)
                 dt=time.time()-now
                 now = time.time()
-                log_str = '%.3f,%.3f,%s\n'%(now,dt*1000,pressure_str)
                 logfile.write(log_str)
     except KeyboardInterrupt:
         print "Caught keyboard interrupt; exiting."
