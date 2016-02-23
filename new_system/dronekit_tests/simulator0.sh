4af0b7b8bad265fc4a3da8ff3fbdd44db40d84da
Mike <mklinker@mit.edu>
2016-02-23 12:05:47 -0500

    Started work on dronekit end to end simulation

---
+++ new_system/dronekit_tests/simulator0.sh
@@ -0,0 +1,13 @@
+#### Simulator Start Script
+
+cd ../../../ardupilot/ArduPlane/
+sim_vehicle.sh -j4 -I 0 --map --console --out 0.0.0.0:14450
+
+
+#### Manually Enter These Commands
+# wp load ../Tools/autotest/bos_mission.txt
+# arm throttle
+# mode auto
+
+
+
---
+++ new_system/dronekit_tests/simulator1.sh
@@ -0,0 +1,13 @@
+#### Simulator Start Script
+
+cd ../../../ardupilot/ArduPlane/
+sim_vehicle.sh -j4 -I 1 --map --console --out 0.0.0.0:14451
+
+
+#### Manually Enter These Commands
+# wp load ../Tools/autotest/bos_mission.txt
+# arm throttle
+# mode auto
+
+
+
