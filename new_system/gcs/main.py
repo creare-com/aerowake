50ef2ddea025a5b72693b3e761ebc98d2675f74d
Mike <mklinker@mit.edu>
2016-03-14 01:04:15 -0400

    user interface

--- new_system/gcs/main.py
+++ new_system/gcs/main.py
@@ -33,23 +33,6 @@ gcs_connect_path = '127.0.0.1:14556'
 
 
 
-## PUT THIS IN A WORKER THREAD
-
-from Tkinter import *
-
-root = Tk()
-
-def callback(event):
-    print "clicked at", event.x, event.y
-
-frame = Frame(root, width=100, height=100)
-frame.bind("<Button-1>", callback)
-frame.pack()
-
-root.mainloop()
-
-
-
 ####!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Local System Setup !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 #### Logging Setup. ####
