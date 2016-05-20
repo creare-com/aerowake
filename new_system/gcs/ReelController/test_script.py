8489746b5f0ca0651a53c4eb0303516c3da6e02d
John Walthour <jdw@creare.com>
2016-05-20 12:37:27 -0400

    Let's do this the other way

--- new_system/gcs/ReelController/test_script.py
+++ new_system/gcs/ReelController/test_script.py
@@ -1,14 +1,12 @@
-# Meant to be pasted into an ipython console
+# Run as ipython -i test_script.py
 import time
 import logging
 logging.basicConfig(level=logging.INFO)
 import ReelController
 rc = ReelController.ReelController()
-rc.getTetherLengthM()
-rc._home_pos_m
 def setAndLoop(len_m):
-rc.setTetherLengthM(len_m)
-while True:
-rc.update()
-time.sleep(0.05)
+    rc.setTetherLengthM(len_m)
+    while True:
+        rc.update()
+        time.sleep(0.05)
 
