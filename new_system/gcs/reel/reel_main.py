8991a1c4b4527245eed2ca3612070a71b96bfd70
John Walthour <jdw@creare.com>
2016-05-26 10:20:54 -0400

    Revert "Easier to leave this functionality in reel.py"
    This reverts commit 086a2ce221451d5d56ba0706adb78de4432916fb.

---
+++ new_system/gcs/reel/reel_main.py
@@ -0,0 +1,26 @@
+import datetime
+import time
+
+class reel_main:
+    def __init__(self,dt):
+        
+
+        # USER CONFIGURABLE SETTINGS
+        self.dt_des = dt
+        time.sleep(.1)
+
+
+    ## Functions
+    
+
+################################################################################
+################################################################################
+
+#This will get called at 200hz or whatever rate you want it to, as specified by the class reel_run().
+
+    def run(self):
+        reel_data = [0,0,0,0,0]        
+        #log data
+
+        return reel_data
+
