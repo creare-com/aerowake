8060deb845b7f31f9e4e8dd6ff874d87b9fc7e23
Michael Klinker <mklinker@mit.edu>
2016-05-26 11:25:12 -0400

    Added two test functions

--- new_system/dronekit_tests/main1.py
+++ new_system/dronekit_tests/main1.py
@@ -16,10 +16,54 @@ from Queue import Empty
 from dronekit import APIException, VehicleMode, connect, mavutil
 
 
-# Autopilot Connection Path. UDP for local simulation. 
-autopilot_connect_path = 'udpin:0.0.0.0:14551'
-#autopilot_connect_path = '/dev/ttyAMA0' #also set baud=57600
-#autopilot_connect_path = '/dev/ttyUSB0'
+gcs_connect_path = '/dev/ttyS0' #USe for RaspPi3
+gcs_baud = 115200
+
+
+
+
+#####!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#####!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! SYSTEM SETUP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#####!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+####!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Local System Setup !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+#### Logging Setup. ####
+
+# Single Log Format:
+# logging.basicConfig(filename='system.log',format='%(relativeCreated)s,%(levelname)s: %#(message)s',level=logging.DEBUG)
+
+print("------------ STARTING AEROWAKE SYSTEM ------------")
+print("-------------------- UAV NODE 0 ------------------")
+
+
+####!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Pixhawk System Setup !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+#### GCS Connection ####
+logging.info("Waiting for GCS")
+while True:
+    try:
+        gcs = connect(gcs_connect_path,baud=gcs_baud,heartbeat_timeout=60, rate=20, wait_ready=True)
+        break
+    except OSError:
+        logging.critical("Cannot find device, is the GCS connected? Retrying...")
+        time.sleep(5)
+    except APIException:
+        logging.critical("GCS connection timed out. Retrying...")
+logging.info("GCS Connected")
+
+
+
+
+while True:
+    time.sleep(.1)
+
+    print "============="
+    print gcs.attitude
 
 
 
