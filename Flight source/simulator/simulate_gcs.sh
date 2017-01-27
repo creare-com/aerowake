0683faa90c111ea5cbb89b9c1e6a5f0e3fbfa44c
John Walthour <jdw@creare.com>
2017-01-27 14:52:19 -0500

    +kill command

--- Flight source/simulator/simulate_gcs.sh
+++ Flight source/simulator/simulate_gcs.sh
@@ -5,7 +5,7 @@ bash _run_gcs.sh&
 # Rover Simulator (ArduRover 3.0.0)
 cd ../../../ardupilot/APMrover2/
 bash sim_vehicle.sh -j4 -I 2 --map --console --out 127.0.0.1:14554 --out 127.0.0.1:14556
-
+kill %1 # end the _run_gcs command
 # map and console
 
 #### Manually Enter These Commands
--- Flight source/simulator/simulate_uav.sh
+++ Flight source/simulator/simulate_uav.sh
@@ -5,6 +5,7 @@ bash _run_uav.sh&
 # UAV Simulator Script (ArduCopter 3.4dev)
 cd ../../../ardupilot/ArduCopter/
 bash sim_vehicle.sh -j4 -I 1 --map --console --out 127.0.0.1:14552
+kill %1 # end the _run_uav command
 
 # FOLLOW THE ARDUPILOT SITL INSTRUCTIONS TO SET UP
 
