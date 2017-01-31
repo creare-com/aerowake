b2c5831ad22e7f77d38ba8e4988e0898d003f4d1
John Walthour <jdw@creare.com>
2017-01-31 09:37:18 -0500

    We'd been relying on uncommitted changes to the "ardupilot" codebase to simulate.  Removed that dependency.

--- Flight source/simulator/simulate_gcs.sh
+++ Flight source/simulator/simulate_gcs.sh
@@ -3,8 +3,10 @@
 bash _run_gcs.sh&
 
 # Rover Simulator (ArduRover 3.0.0)
+# Note that for this to work, you must have the ardupilot git repo cloned in the
+# same directory as the aerowake repo.
 cd ../../../ardupilot/APMrover2/
-bash sim_vehicle.sh -j4 -I 2 --map --console --out 127.0.0.1:14554 --out 127.0.0.1:14556
+bash ../Tools/autotest/sim_vehicle.sh -j4 -I 2 --map --console --out 127.0.0.1:14554 --out 127.0.0.1:14556
 kill %1 # end the _run_gcs command
 # map and console
 
--- Flight source/simulator/simulate_uav.sh
+++ Flight source/simulator/simulate_uav.sh
@@ -3,8 +3,10 @@
 bash _run_uav.sh&
 
 # UAV Simulator Script (ArduCopter 3.4dev)
+# Note that for this to work, you must have the ardupilot git repo cloned in the
+# same directory as the aerowake repo.
 cd ../../../ardupilot/ArduCopter/
-bash sim_vehicle.sh -j4 -I 1 --map --console --out 127.0.0.1:14552
+bash ../Tools/autotest/sim_vehicle.sh -j4 -I 1 --map --console --out 127.0.0.1:14552
 kill %1 # end the _run_uav command
 
 # FOLLOW THE ARDUPILOT SITL INSTRUCTIONS TO SET UP
