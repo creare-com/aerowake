d97af4d891f7c982ea51edb35d0fc4d11be6a2d2
Brandon Draper <draper@mit.edu>
2017-10-19 16:04:55 -0400

    Improved description of mission format and convention

--- Flight source/gcs/mission.py
+++ Flight source/gcs/mission.py
@@ -1,16 +1,21 @@
-# Simple test of branch and commit - Brandon
+# The mission is specified in spherical coordinates with angles in radians and length in meters. Each of the lists below must be of the same length. 
 
-# Simple test #2 of branch and commit - Brandon
+# The convention is as follows:
+#   The origin is defined as the GCS.
+#   THETA : elevation angle defined from vertical : 90 degrees is horizontal (same altitude as GCS) : 0 degrees is vertical (directly above GCS)
+#   PHI : azimuth angle defined from GCS longitudinal axis : 0 degrees is directly behind the GCS : 90 degrees is directly right of the GCS when facing in direction of GCS
 
-# This is how the mission is currently specified. Angles are in Radians. 
-# You must re-run the GCS application to reload this file.
-# Theta: 90 degrees is horizontal ( same altitude as boat )
-# Phi: 0 degrees is straight behind the boat. This is always relative to the stern of the ship.
-# This system can be replaced with a mission file or similiar. 
+# The general model of this file is: 
 # THETA  = [1.2, 1.2, 1.4, 1.2,  1.3, 1.3, 1.3, 1.3]
 # PHI    = [  0,  .5,   0, -.5,  -.5,   0,  .5,   0]
 # L      = [ 50,  50,  50,  60,   60,  70,  70, 100] 
+
+# You must re-run the GCS main.py to reload this file.
+
 from math import pi
+# THETA  = [ 0, pi/4,  pi/4,  pi/4,  pi/4, ]
+# PHI    = [ 0,    0, +pi/4, -pi/4,     0, ]
+# L      = [50,   50,    50,    50,    50, ] 
 THETA  = [ 0, pi/4,  pi/4,  pi/4,  pi/4, ]
 PHI    = [ 0,    0, +pi/4, -pi/4,     0, ]
 L      = [50,   50,    50,    50,    50, ] 
