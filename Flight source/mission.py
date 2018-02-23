f4ee3d147012870a2182b24ea671a9495303aab5
Brandon Draper <draper@mit.edu>
2018-02-23 15:42:24 -0500

    Added a few more mission options.

--- Flight source/mission.py
+++ Flight source/mission.py
@@ -39,3 +39,19 @@ wp_E = list(center_E) + list(basic_E) + list(end_E) + list(reversed(basic_E)) + 
 wp_N = [get_y3(x,20) for x in wp_E]
 wp_D = [-10]*len(wp_N)
 num_wp = [len(wp_N)]
+
+# # Mission 4: Abbreviated arc at Brigg's field
+# def get_y4(x,r):
+# 	return round(np.sqrt(r**2 - x**2),2)
+# wp_E = [0, 4.5,8.5,4.5,0,-4.5,-8.5,-4.5,0]
+# wp_N = [get_y4(x,12) for x in wp_E]
+# wp_D = [-2]*len(wp_N)
+# num_wp = [len(wp_N)]
+
+# # Mission 5: Abbreviated arc at stadium
+# def get_y5(x,r):
+# 	return round(np.sqrt(r**2 - x**2),2)
+# wp_E = [0, 5.67,10.6,5.67,0,-5.67,-10.6,-5.67,0]
+# wp_N = [get_y5(x,15) for x in wp_E]
+# wp_D = [-2]*len(wp_N)
+# num_wp = [len(wp_N)]
