a3840d4a4e73e881ca7e6b4a7413a2b2f756076f
Swarm Blue <acl-swarm@mit.edu>
2018-05-25 09:10:46 -0400

    Added waypoint to simple mission .

--- Flight source/mission_rot.py
+++ Flight source/mission_rot.py
@@ -1,6 +1,6 @@
 # This mission file created using rotate_mission.py, a part of the Creare AeroWake project.
 
-wp_N = [14.14214, 12.72792]
-wp_E = [14.14214, 12.72792]
-wp_D = [-5, -5]
+wp_N = [14.14214, 12.72792, 11.31371]
+wp_E = [14.14214, 12.72792, 14.14214]
+wp_D = [-5, -5, -5]
 num_wp = [len(wp_N)]
