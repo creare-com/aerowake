6e3e26b6f28f91e9e114943483a5e6af92747b25
Brandon <draper@mit.edu>
2018-06-08 17:11:31 -0400

    Adding automatic rosbag functionality.

--- ros-related/scripts/record_bags.py
+++ ros-related/scripts/record_bags.py
@@ -7,20 +7,23 @@ from sensor_msgs.msg import Image
 def cbImageRecord(data):
 	global i
 	# Camera will nominally operate at 15 hz, so i%val = image rate in rosbag.
-	val = 7.5
+	val = 1
 	if i%val == 0:
-		bag.write('/camera/image_raw', data)
+		if i < 61:
+			bag.write('/camera/image_mono', data)
+		else:
+			print 'Done'
 	i = i + 1
 
 def listener():
-	rospy.init_node('listener', anonymous=True)
-	rospy.Subscriber('/camera/image_raw', Image, cbImageRecord)
+	rospy.init_node('imageRecordListener', anonymous=True)
+	rospy.Subscriber('/camera/image_mono', Image, cbImageRecord)
 	rospy.spin()
 
 if __name__ == '__main__':
 	global i
 	i = 0
-	bag = rosbag.Bag('rest.bag', mode='w', compression='bz2')
+	bag = rosbag.Bag('testc.bag', mode='w', compression='lz4')
 	listener()
 	while not rospy.is_shutdown():
 		pass
