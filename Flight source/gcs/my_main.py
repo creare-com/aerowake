875b8997b25ca54e184d7222fbd3d0d89d309c8a
Brandon Draper <draper@mit.edu>
2018-06-21 10:34:58 +0000

    Changed log directory to ~/logs/aerowake-logs/

--- Flight source/gcs/my_main.py
+++ Flight source/gcs/my_main.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
 	logger = logging.getLogger('gcs_logger')
 	logger.setLevel(logging.DEBUG)
 	# Create file handler that sends all logger messages (DEBUG and above) to file
-	fh = logging.FileHandler('%s/.ros/aerowake-logs/gcs-%s.log' %(os.path.expanduser('~'),time.strftime('%m-%d-%Hh-%Mm-%Ss', time.localtime())))
+	fh = logging.FileHandler('%s/logs/aerowake-logs/gcs-%s.log' %(os.path.expanduser('~'),time.strftime('%m-%d-%Hh-%Mm-%Ss', time.localtime())))
 	fh.setLevel(logging.DEBUG)
 	# Create console handler that sends some messages (INFO and above) to screen
 	ch = logging.StreamHandler(sys.stdout)
