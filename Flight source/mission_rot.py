47d7d6be68f69ba81e9646739840fb037e95a813
Ryan Scerbo <scerbo@mit.edu>
2018-06-25 17:47:17 -0400

    switched current mission to mission 1

--- Flight source/mission_rot.py
+++ Flight source/mission_rot.py
@@ -1,6 +1,6 @@
 # This mission file created using rotate_mission.py, a part of the Creare AeroWake project.
 
-wp_N = [8.45237, 6.33927, 1.80773]
-wp_E = [18.12616, 13.59462, 15.70771]
-wp_D = [-5, -5, -8]
+wp_N = [-14.14214, -21.2132, -14.14214, -7.07107, -14.14214]
+wp_E = [14.14214, 7.07107, 14.14214, 21.2132, 14.14214]
+wp_D = [-10, -10, -10, -10, -10]
 num_wp = [len(wp_N)]
