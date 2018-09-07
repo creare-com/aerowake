364caab74edcdca5d18ff79db307851522a2acd6
Brandon Draper <draper@mit.edu>
2018-09-07 21:40:57 +0000

    Disabled reel and set new mission.

--- Flight source/base_mission.py
+++ Flight source/base_mission.py
@@ -1,7 +1,7 @@
 # This mission file created using rotate_mission.py, a part of the Creare AeroWake project.
 
 alt_takeoff = 5
-wp_N = [10, 10, 10, 10]
-wp_E = [0, 5, 0, -5]
-wp_D = [-10.0, -10.0, -10.0, -10.0]
+wp_N = [20.0, 18.71, 14.14, 18.71, 20.0, 18.71, 14.14, 18.71]
+wp_E = [0.0, 7.07, 14.14, 7.07, 0.0, -7.07, -14.14, -7.07]
+wp_D = [-10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, -10.0]
 num_wp = len(wp_N)
