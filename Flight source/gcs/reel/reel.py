a6a2e2671f77d1caff30dd3cc5ad29fce4a1ca41
draper <draper@mit.edu>
2018-09-28 12:19:42 -0400

    Modified reel.py to create its own logger. ReelController.py calls this logger as logger.info('...') instead of using logging.info('...')

--- Flight source/gcs/reel/reel.py
+++ Flight source/gcs/reel/reel.py
@@ -21,7 +21,24 @@ class reel_run (Process):
         self._cycles = 0
 
         # Log Setup
-        self._logger = logging.getLogger('gcs_logger')
+        self._logger = logging.getLogger('reel_logger')
+        self._logger.setLevel(logging.DEBUG)
+        # Create file handler that sends all logger messages (DEBUG and above) to file
+        logfile = '%s/logs/gcs-logs/gcs-%s.log' %(os.path.expanduser('~'),time.strftime('%Y-%m-%d-%Hh-%Mm-%Ss', time.localtime()))
+        fh = logging.FileHandler(logfile)
+        print 'Logging GCS data to %s' %(logfile)
+        fh.setLevel(logging.DEBUG)
+        # Create console handler that sends some messages (INFO and above) to screen
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setLevel(logging.INFO)
+        # Set the log format for each handler
+        form_fh = logging.Formatter('%(created)s,%(relativeCreated)s,%(funcName)s,%(levelname)s: %(message)s')
+        form_ch = logging.Formatter('%(levelname)s: %(message)s')
+        fh.setFormatter(form_fh)
+        ch.setFormatter(form_ch)
+        # Add the handler to the logger
+        self._logger.addHandler(fh)
+        self._logger.addHandler(ch)
 
     def run(self):
         run = True
