aec6efd533d9fd17ab0ca26a2991697cf55ed53a
draper <draper@mit.edu>
2018-09-28 13:41:16 -0400

    Fixing logging.

--- Flight source/gcs/reel/reel.py
+++ Flight source/gcs/reel/reel.py
@@ -45,60 +45,65 @@ class reel_run (Process):
     def run(self):
         run = True
         self._rc = ReelController.ReelController()
-        while run:
-            #check if any new cmds
-            try:
-                cmd = self._cmd.get(False)
-            except Empty:
-                cmd = None
-
-            # We expect cmd to be one of the following:
-            # {"cmd":"goto", "L":<length in meters, an int}
-            # {"cmd":"halt"}    # holds the reel where it is
-            # {"cmd":"rehome"}  # consider the current tether length to be 0m
-            # {"cmd":"exit"}    # exit the subprocess
-            if cmd != None and cmd.has_key('cmd'):
-                if cmd['cmd'] == 'goto':
-                    L = cmd['L']
-                    self._logger.info("reel.py: Setting tether length to %0.01f m"%L)
-                    self._rc.setTetherLengthM(L)
-                elif cmd['cmd'] == 'rehome':
-                    self._logger.info("reel.py: Homing tether\n")
-                    self._rc.youAreHome()
-                elif cmd['cmd'] == 'halt':
-                    self._logger.info("reel.py: Halting reel via halt\n")
-                    self._rc.stopMoving()
-                elif cmd['cmd'] == 'exit':
-                    self._logger.info("reel.py: Halting reel via exit\n")
-                    self._rc.stopMoving()
-                    del self._rc
-                    run = False
-                    continue
-            
-            t_0 = datetime.datetime.now()
-
-            # Push tether status back if we've skipped the appropriate number of cycles
-            self._rc.update()
-            if(self._cycles >= SKIP_CYCLES):
-                L = self._rc.getTetherLengthM()
-                T = self._rc.getTetherTensionN()
+        try:
+            while run:
+                #check if any new cmds
                 try:
-                    self._data_out.get(False)
+                    cmd = self._cmd.get(False)
                 except Empty:
-                    # GCS main code will occasionally pull out this data, so we need to protect against an empty Queue
-                    pass
-                self._data_out.put({"L": L, "T": T})
-                self._cycles = 0
-            else:
-                self._cycles += 1
+                    cmd = None
 
-            #Sleep for desired amount of time
-            t_1 = datetime.datetime.now()
-            dt = (t_1-t_0).total_seconds()
-            sleep_time = self._dt_des-dt
-            if sleep_time<0:
-                sleep_time = 0
-            time.sleep(sleep_time)
+                # We expect cmd to be one of the following:
+                # {"cmd":"goto", "L":<length in meters, an int}
+                # {"cmd":"halt"}    # holds the reel where it is
+                # {"cmd":"rehome"}  # consider the current tether length to be 0m
+                # {"cmd":"exit"}    # exit the subprocess
+                if cmd != None and cmd.has_key('cmd'):
+                    if cmd['cmd'] == 'goto':
+                        L = cmd['L']
+                        self._logger.info("reel.py: Setting tether length to %0.01f m"%L)
+                        self._rc.setTetherLengthM(L)
+                    elif cmd['cmd'] == 'rehome':
+                        self._logger.info("reel.py: Homing tether\n")
+                        self._rc.youAreHome()
+                    elif cmd['cmd'] == 'halt':
+                        self._logger.info("reel.py: Halting reel via halt\n")
+                        self._rc.stopMoving()
+                    elif cmd['cmd'] == 'exit':
+                        self._logger.info("reel.py: Halting reel via exit\n")
+                        self._rc.stopMoving()
+                        del self._rc
+                        run = False
+                        continue
+                
+                t_0 = datetime.datetime.now()
+
+                # Push tether status back if we've skipped the appropriate number of cycles
+                self._rc.update()
+                if(self._cycles >= SKIP_CYCLES):
+                    L = self._rc.getTetherLengthM()
+                    T = self._rc.getTetherTensionN()
+                    try:
+                        self._data_out.get(False)
+                    except Empty:
+                        # GCS main code will occasionally pull out this data, so we need to protect against an empty Queue
+                        pass
+                    self._data_out.put({"L": L, "T": T})
+                    self._cycles = 0
+                else:
+                    self._cycles += 1
+
+                #Sleep for desired amount of time
+                t_1 = datetime.datetime.now()
+                dt = (t_1-t_0).total_seconds()
+                sleep_time = self._dt_des-dt
+                if sleep_time<0:
+                    sleep_time = 0
+                time.sleep(sleep_time)
+        except KeyboardInterrupt:
+            self._logger.info('Reel got CTRL+C. Stopping reel.')
+            self._rc.stopMoving()
+            del self._rc
 
 
 
