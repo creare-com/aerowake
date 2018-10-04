1e94b5969df21459001c64e0d16a23a6bfddc931
Brandon Draper <draper@mit.edu>
2018-10-04 18:58:55 -0400

    Added delay before call to tail and to bagging.

--- preflight.sh
+++ preflight.sh
@@ -26,6 +26,7 @@ else
 	screen -S yaw-cmd -p 0 -X stuff "roslaunch creare yaw_commanding.launch^M"
 	screen -S flight-cmd -p 0 -X stuff "roslaunch aerowake flight_companion.launch filename:=$FILENAME^M"
 	screen -S airprobe -p 0 -X stuff "nice -n -10 python ~/creare_ws/src/aerowake/aerowake_git/Flight\ source/uav/airprobe/airprobe_main.py $FILENAME^M"
+	sleep 1 # airprobe and yaw-cmd need time to initialize
 	screen -S probe-check -p 0 -X stuff "tail -f /crearedrive/airprobe-logs/$FILENAME*^M"
 
 	# Preload commands in these screens, but do not issue them
