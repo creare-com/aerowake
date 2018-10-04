6155ed08f5b98dbebc0fb0549df26dfaff8df2f9
Brandon Draper <draper@mit.edu>
2018-10-04 17:35:32 -0400

    Airprobe and flight-cmd now autostart.

--- preflight.sh
+++ preflight.sh
@@ -17,8 +17,8 @@ screen -dmS probe-check
 screen -S roscore -p 0 -X stuff "roscore^M"
 sleep 3 # roscore needs time to initialize
 screen -S yaw-cmd -p 0 -X stuff "roslaunch creare yaw_commanding.launch^M"
-screen -S flight-cmd -p 0 -X stuff "roslaunch aerowake flight_companion.launch filename:=$FILENAME"
-screen -S airprobe -p 0 -X stuff "nice -n -10 python ~/creare_ws/src/aerowake/aerowake_git/Flight\ source/uav/airprobe/airprobe_main.py"
+screen -S flight-cmd -p 0 -X stuff "roslaunch aerowake flight_companion.launch filename:=$FILENAME^M"
+screen -S airprobe -p 0 -X stuff "nice -n -10 python ~/creare_ws/src/aerowake/aerowake_git/Flight\ source/uav/airprobe/airprobe_main.py^M"
 
 # Preload commands in these screens, but do not issue them
 screen -S bags -p 0 -X stuff "rosbag record --split --size=3000 --lz4 -a -x /camera/image_raw -o /crearedrive/$FILENAME"
