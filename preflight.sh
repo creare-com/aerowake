4402c531d006e987cf8e7ab6bb9d2ae04df0c4fd
draper <draper@mit.edu>
2018-10-04 17:12:49 -0400

    Startup script to make my life easier before each flight.

---
+++ preflight.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+FILENAME=$1
+
+# Kill all existing screens
+killall screen
+
+# Start all screen sessions in detached mode
+screen -dmS roscore
+screen -dmS yaw-cmd
+screen -dmS flight-cmd
+screen -dmS bags
+screen -dmS airprobe
+screen -dmS probe-check
+
+# Issue commands in these screens
+screen -S roscore -p 0 -X stuff "roscore^M"
+sleep 2 # roscore needs time to initialize
+screen -S yaw-cmd -p 0 -X stuff "roslaunch creare yaw_commanding.launch^M"
+screen -S flight-cmd -p 0 -X stuff "roslaunch aerowake flight_companion.launch filename:=$FILENAME"
+screen -S airprobe -p 0 -X stuff "nice -n -10 python ~/creare_ws/src/aerowake/aerowake_git/Flight\ source/uav/airprobe/airprobe_main.py"
+
+# Preload commands in these screens, but do not issue them
+screen -S bags -p 0 -X stuff "rosbag record --split --size=3000 --lz4 -a -x /camera/image_raw -o /crearedrive/$filename"
+screen -S probe-check -p 0 -X stuff "tail -f /crearedrive/airprobe-logs/pressure"
