fe849ca5d7492fda3e80505544f107eb9ed89edd
Brandon Draper <draper@mit.edu>
2018-10-04 21:16:05 -0400

    Removed bagging screen since bagging moved to uav/main.py upon arming.

--- preflight.sh
+++ preflight.sh
@@ -16,7 +16,6 @@ else
 	screen -dmS roscore
 	screen -dmS yaw-cmd
 	screen -dmS flight-cmd
-	screen -dmS bags
 	screen -dmS airprobe
 	screen -dmS probe-check
 
@@ -26,10 +25,7 @@ else
 	screen -S yaw-cmd -p 0 -X stuff "roslaunch creare yaw_commanding.launch^M"
 	screen -S flight-cmd -p 0 -X stuff "roslaunch aerowake flight_companion.launch filename:=$FILENAME^M"
 	screen -S airprobe -p 0 -X stuff "nice -n -10 python ~/creare_ws/src/aerowake/aerowake_git/Flight\ source/uav/airprobe/airprobe_main.py $FILENAME^M"
-	sleep 1 # airprobe and yaw-cmd need time to initialize
+	sleep 1 # airprobe needs time to initialize
 	screen -S probe-check -p 0 -X stuff "tail -f /crearedrive/airprobe-logs/$FILENAME*^M"
-
-	# Preload commands in these screens, but do not issue them
-	screen -S bags -p 0 -X stuff "rosbag record --split --size=3000 --lz4 -a -x /camera/image_raw -o /crearedrive/$FILENAME"
 
 fi
