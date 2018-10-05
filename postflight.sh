f654fee108a40b90ab3cea2469a2e5fa972d32b8
Brandon Draper <draper@mit.edu>
2018-10-04 21:30:20 -0400

    Added graceful exit to all screens. Also added killall screen after graceful exit.

--- postflight.sh
+++ postflight.sh
@@ -2,6 +2,12 @@
 
 # Gracefully stop programs before killing screens
 screen -S roscore -p 0 -X stuff "^C"
+screen -S yaw-cmd -p 0 -X stuff "^C"
+screen -S flight-cmd -p 0 -X stuff "^C"
+screen -S airprobe -p 0 -X stuff "^C"
+screen -S probe-check -p 0 -X stuff "^C"
 
-# Kill all screens
-# killall screen
+# Kill all screens after allowing ^C to process
+sleep 2
+killall screen
+
