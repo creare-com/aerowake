1f9e4650638a2b962d78efed73e6510ed5d29073
draper <draper@mit.edu>
2018-10-09 15:38:22 -0400

    Added auto-unmounting of crearedrive.

--- postflight.sh
+++ postflight.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Gracefully stop programs before killing screens
+# Gracefully stop programs
 screen -S roscore -p 0 -X stuff "^C"
 screen -S yaw-cmd -p 0 -X stuff "^C"
 screen -S flight-cmd -p 0 -X stuff "^C"
 screen -S airprobe -p 0 -X stuff "^C"
 screen -S probe-check -p 0 -X stuff "^C"
 
-# Kill all screens after allowing ^C to process
-sleep 2
-killall screen
+# Unmount crearedrive
+sleep 2 # Allow ^C to process
+umount /crearedrive
 
+FLAGUNMOUNTED="$(lsblk)"
+if ! echo "$FLAGUNMOUNTED" | grep -q "crearedrive"
+then
+	echo "/crearedrive unmounted successfully"
+else
+	echo "/crearedrive needs to be unmounted"
+fi
