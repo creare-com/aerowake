9b5c594acb1507a725d9c7676de1842265b67831
draper <draper@mit.edu>
2018-10-09 20:49:54 -0400

    Moved logging statements inside conditional.

--- Flight source/uav/main.py
+++ Flight source/uav/main.py
@@ -259,26 +259,26 @@ class DroneCommanderNode(object):
 						# Tell the UAV to continue following the current waypoint through the standard pipeline
 						gcs.parameters[cmd_param] = current_wp
 					elif command == 202:
-						logger.info('Increasing WPNAV_SPEED by 0.5 m/s to %s' %(new_speed))
-						logger.debug('WPNAV_SPEED,%s' %(uav.parameters['WPNAV_SPEED']))
 						new_speed = uav.parameters['WPNAV_SPEED'] + 50
 						if new_speed < 2000:
+							logger.info('Increasing WPNAV_SPEED by 0.5 m/s to %s' %(new_speed))
+							logger.debug('WPNAV_SPEED,%s' %(uav.parameters['WPNAV_SPEED']))
 							uav.parameters['WPNAV_SPEED'] = new_speed # 20-2000 by 50 [cm/s]
 						# Tell the UAV to continue following the current waypoint through the standard pipeline
 						gcs.parameters[cmd_param] = current_wp
 					elif command == 203:
-						logger.info('Increasing WPNAV_SPEED by 0.5 m/s to %s' %(new_speed))
-						logger.debug('WPNAV_SPEED,%s' %(uav.parameters['WPNAV_SPEED']))
 						new_speed = uav.parameters['WPNAV_SPEED'] - 50
 						if new_speed > 20:
+							logger.info('Increasing WPNAV_SPEED by 0.5 m/s to %s' %(new_speed))
+							logger.debug('WPNAV_SPEED,%s' %(uav.parameters['WPNAV_SPEED']))
 							uav.parameters['WPNAV_SPEED'] = new_speed # 20-2000 by 50 [cm/s]
 						# Tell the UAV to continue following the current waypoint through the standard pipeline
 						gcs.parameters[cmd_param] = current_wp
 					elif command == 204:
-						logger.info('Increasing WPNAV_ACCEL by 0.3 m/s/s to %s' %(new_accel))
-						logger.debug('WPNAV_ACCEL,%s' %(uav.parameters['WPNAV_ACCEL']))
 						new_accel = uav.parameters['WPNAV_ACCEL'] + 30
 						if new_accel < 500:
+							logger.info('Increasing WPNAV_ACCEL by 0.3 m/s/s to %s' %(new_accel))
+							logger.debug('WPNAV_ACCEL,%s' %(uav.parameters['WPNAV_ACCEL']))
 							uav.parameters['WPNAV_ACCEL'] = new_accel # 50-500 by 10 [cm/s/s]
 						# Tell the UAV to continue following the current waypoint through the standard pipeline
 						gcs.parameters[cmd_param] = current_wp
@@ -286,7 +286,7 @@ class DroneCommanderNode(object):
 						new_accel = uav.parameters['WPNAV_ACCEL'] - 30
 						if new_accel < 50:
 							logger.info('Decreasing WPNAV_ACCEL by 0.3 m/s/s to %s' %(new_accel))
-						logger.debug('WPNAV_ACCEL,%s' %(uav.parameters['WPNAV_ACCEL']))
+							logger.debug('WPNAV_ACCEL,%s' %(uav.parameters['WPNAV_ACCEL']))
 							uav.parameters['WPNAV_ACCEL'] = new_accel # 50-500 by 10 [cm/s/s]
 						# Tell the UAV to continue following the current waypoint through the standard pipeline
 						gcs.parameters[cmd_param] = current_wp
