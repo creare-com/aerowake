352b84e0217c77241ba3f67223673a507dc4c0bd
John Walthour <jdw@creare.com>
2019-05-28 11:21:43 -0400

    Switched to a makefile

--- Flight source/FlightRecorder/make.sh
+++
@@ -1,7 +0,0 @@
-FILES="src/*.cpp"
-INC="-I/usr/include/spinnaker -Iinclude/"
-OUTPUT="FlightRecorder"
-OPTIONS="-std=c++11"
-LIBS="-lSpinnaker -lopencv_core -lopencv_imgcodecs -lopencv_features2d -lopencv_imgproc"
-
-g++ $FILES -o $OUTPUT $INC $LIBS $OPTIONS
