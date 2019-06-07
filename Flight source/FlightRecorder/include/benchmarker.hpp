c50ece304546188bb076d69288ae72a1c3f36d77
John Walthour <jdw@creare.com>
2019-06-07 12:32:34 -0400

    Filled in proper documentation

--- Flight source/FlightRecorder/include/benchmarker.hpp
+++ Flight source/FlightRecorder/include/benchmarker.hpp
@@ -3,6 +3,12 @@
  * 
  * Simple, very manually-controlled profiler object.
  * 
+ * 2017-02-28	JDW	Created.
+ * 2019-06-07	JDW	Refactored into a header-only library
+ * 
+ * 
+ * 
+ * 
  * Usage example:
  * 
  * list<const Benchmarker *> myBms;
@@ -27,8 +33,8 @@
  * Benchmarker::summarizeBenchmarksToStream(myBms, cout);
  * 
  * 
- * 2017-02-28	JDW	Created.
- * 2019-06-07	JDW	Refactored into a header-only library
+ * 
+ * 
 */
 
 #ifndef __BENCHMARKER_HPP__
@@ -67,6 +73,14 @@ public:
 	{;}
 	virtual ~Benchmarker() {;}
 
+	/**
+	 * Summarize a list of benchmarkers to a stream such as std::cout or a log file.
+	 * Entries look like: <name>: <average time of one iteration>[, <average items processed>] (<iteration count>)
+	 * 
+	 * @param allBms A list of benchmarker objects
+	 * @param stream The output stream, such as std::cout or a log file.
+	 * 
+	 */
 	static void summarizeBenchmarksToStream(list<const Benchmarker *> allBms, ostream & stream) {
 		for(auto bm = allBms.begin(); bm != allBms.end(); ++bm) {
 			stream << endl << (*bm)->getName() << ": "  << (*bm)->getAvgMs() << "ms avg";
@@ -77,47 +91,94 @@ public:
 		}
 		stream << endl;
 	}
-	// An iteration of processing is about to begin
+	
+	/**
+	 * Call this to begin timing one iteration of processing.
+	 * The next call should be to pause() or stop().
+	 */
 	void start() {
 		startTime = chrono::steady_clock::now();
 	}
-	// Processing of this type has paused, but processing for this iteration is not finished.
+
+	/**
+	 * Call this to pause timing one iteration of processing.
+	 * Call this only after calling start().  The next call should be to resume().
+	 * 
+	 * @param items_processed (optional) The number of items that have been processed since the call to start()
+	 */
 	void pause(int items_processed = 0)	{
 		chrono::steady_clock::duration elapsed = (chrono::steady_clock::now() - startTime);
 		totalDuration += elapsed;
 		numItemsProcessed += items_processed;
 	}
-	// Processing of this type is about to resume.  Equivalent to start().
-	void resume(){
+	
+	/**
+	 * Call this to resume timing of processing.
+	 * Call this only after calling pause().
+	 * The next call should be to pause() or stop().
+	 */
+	void resume() {
 		start(); 
 	}
-	// An iteration of processing has concluded. Optionally, it processed N items.
-	// Equivalent to pause() followed by conclude().
+	
+	/**
+	 * Call this to conclude timing one iteration of processing.
+	 * Call this only after calling start() or resume().
+	 * 
+	 * @param items_processed (optional) The number of items that have been processed since the call to start() or the last call to resume()
+	 */
 	void end(int items_processed = 0) {
 		pause(items_processed); // Stops timekeeping
-		conclude(); // End of this iteration
-	}
-	// An iteration of processing has concluded.
-	// Use this when paused - it indicates the end of an iteration, without marking
-	// the end time.
-	void conclude() {
 		numTimesRun++; // End of this iteration
 	}
-	// Get the total time spent in processing, divided by the number of iterations ran.
+
+	/**
+	 * Get the average duration of a single iteration.
+	 * 
+	 * @return the total time spent in processing, divided by the number of iterations ran.
+	 */
 	chrono::steady_clock::duration getAvgTime() const {
 		return (numTimesRun > 0) ? (totalDuration / numTimesRun) : chrono::milliseconds(0);
 	}
 
-	// Get the total time spent in processing, divided by the number of iterations ran.
+	/**
+	 * Get the average duration of a single iteration, in milliseconds.
+	 * 
+	 * @return the total time spent in processing, in ms, divided by the number of iterations ran.
+	 */
 	double getAvgMs() const {
 		return chrono::duration_cast<chrono::milliseconds>(getAvgTime()).count();
 	}
-
+	
+	/**
+	 * Set the human-readable name of the benchmarker.
+	 * 
+	 * @param name the human-readable name of the benchmarker.
+	 */
 	void setName(string name) { this->name = name; }
+
+	/**
+	 * Get the human-readable name of the benchmarker.
+	 * 
+	 * @return the human-readable name of the benchmarker.
+	 */
 	string getName() const { return name; }
+	
+	/**
+	 * Get the average number of items processed in one iteration.
+	 * This will be zero, unless you have supplied the optional items_processed argument to end() and/or pause().
+	 * 
+	 * @return the average number of items processed in one iteration
+	 */
 	double getAvgItemsProcessed() const { 
 		return numTimesRun > 0 ? numItemsProcessed / numTimesRun : 0; 
 	}
+	
+	/**
+	 * Get the number of iterations, defined as a pair of calls to start() and end().
+	 * 
+	 * @return the number of iterations
+	 */
 	int getIterations() const { return numTimesRun; }
 };
 
