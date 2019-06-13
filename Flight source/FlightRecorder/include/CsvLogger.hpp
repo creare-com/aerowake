4508ef591b86fbdd061faf87722169ffa0643564
John Walthour <jdw@creare.com>
2019-06-13 14:20:07 -0400

    First draft of CSV logger; not fully fleshed out, but it compiles and runs.  Compile with:
    `g++ -o csv_test src/CsvLogger.cpp -Iinclude/ -std=c++11`

---
+++ Flight source/FlightRecorder/include/CsvLogger.hpp
@@ -0,0 +1,103 @@
+/*
+	CsvLogger.hpp
+	
+	Class for saving data to a CSV log.
+	
+	Usage:
+	
+	CsvLogger myLogger;
+	auto col1 = myLogger.addColumn("foo");
+	auto col2 = myLogger.addColumn("bar");
+	myLogger.startNewLogFile();
+	
+	myLogger.logData(CsvLogger::cell(col1, 1));
+	myLogger.logData(vector<CsvLogger::cell>({CsvLogger::cell(col1, 1), CsvLogger::cell(col1, 1)}));
+	
+	
+	2019-06-13	JDW	Created.
+*/
+
+#ifndef __CSVLOGGER_HPP__
+#define __CSVLOGGER_HPP__
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class CsvLogger {
+public:
+	/**
+	 * Constructor.  Will not attempt to open log file.
+	 *
+	 * @param recordingDir The directory in which to save the file(s)
+	 * @param logFilenameFormat Pattern specifying the filename for log data. Will substitute flags found here: https://howardhinnant.github.io/date/date.html#to_stream_formatting.  Avoid any characters not supported by the filesystem, such as colons.
+	 * @param timestampFormat Pattern specifying the timestamp in the first column. Will substitute flags found here: https://howardhinnant.github.io/date/date.html#to_stream_formatting.
+	 */
+	CsvLogger(string recordingDir = ".", string logFilenameFormat = "data_%F_%H-%M-%S.csv", string timestampFormat = "%H:%M:%S") :
+		recordingDir      (recordingDir     ),
+		logFilenameFormat (logFilenameFormat),
+		timestampFormat   (timestampFormat  )
+	{ 
+		headers.push_back("time");
+	}
+	virtual ~CsvLogger() {
+		closeFile();
+	}
+	
+	/**
+	 * Close old log file, if one was open.
+	 * Open a new log file for subsequent calls to logData().
+	 */
+	void startNewLogFile();
+	
+	/**
+	 * Call this before logging data.
+	 * @param header Single-token header
+	 * @return the column ID
+	 */
+	unsigned int addColumn(string header);
+	
+	/**
+	 * A cell in a CSV table
+	 */
+	class Cell {
+	public:
+		unsigned int id;
+		double value;
+		
+		Cell(unsigned int id, double value) : id(id), value(value) { ; }
+		friend bool operator< (const Cell &c1, const Cell &c2) { return c1.value < c2.value; }
+
+	};
+	
+	/**
+	 * Log one timestamped value on its own line.
+	 * @param value Value to log
+	 */
+	void logData(Cell value);
+	/**
+	 * Log a timestamped line with several values
+	 * @param values Values to log
+	 */
+	void logData(vector<Cell> values);
+	
+private:
+	string recordingDir;
+	string logFilenameFormat;
+	string timestampFormat;
+	vector<string> headers;
+	/**
+	 * Open a file based on the timestamp
+	 */
+	void openFile();
+	void closeFile();
+};
+
+#endif // __CSVLOGGER_HPP__
+
---
+++ Flight source/FlightRecorder/src/CsvLogger.cpp
@@ -0,0 +1,94 @@
+/*
+	CsvLogger.hpp
+	
+	Class for saving data to a CSV log.
+	
+	Usage:
+	
+	CsvLogger myLogger;
+	auto col1 = myLogger.addColumn("foo");
+	auto col2 = myLogger.addColumn("bar");
+	myLogger.startNewLogFile();
+	
+	myLogger.logData(CsvLogger::cell(col1, 1));
+	myLogger.logData(vector({CsvLogger::cell(col1, 1), CsvLogger::cell(col1, 1)}));
+	
+	
+	2019-06-13	JDW	Created.
+*/
+#include <CsvLogger.hpp>
+
+using namespace std;
+
+/**
+ * Close old log file, if one was open.
+ * Open a new log file for subsequent calls to logData().
+ */
+void CsvLogger::startNewLogFile() {
+	openFile();
+}
+
+/**
+ * Call this before logging data.
+ * @param header Single-token header
+ * @return the column ID
+ */
+unsigned int CsvLogger::addColumn(string header) {
+	headers.push_back(header);
+	// Column ID just happens to be the index
+	return headers.size() - 1;
+}
+/**
+ * Log one timestamped value on its own line.
+ * @param value Value to log
+ */
+void CsvLogger::logData(Cell value) {
+	logData(vector<Cell>({value}));
+}
+/**
+ * Log a timestamped line with several values
+ * @param values Values to log
+ */
+void CsvLogger::logData(vector<Cell> values) {
+	// Timestamp
+	// TODO
+	cout <<',';
+	unsigned int col = 1; 
+	sort(values.begin(), values.end());
+	auto it = values.begin();
+	for(; col < headers.size(); col++) {
+		if(it != values.end()) {
+			if(it->id == col) {
+				cout << it->value;
+				++it;
+			}
+		}
+		if(col < headers.size() - 1) {
+			cout << ',';
+		}
+	}
+	cout << endl;
+}
+void CsvLogger::openFile() {
+	// TODO: open file
+	for (auto header : headers) {
+		cout << header << ",";
+	}
+	cout << endl;
+}
+void CsvLogger::closeFile() {
+	
+}
+
+
+int main() {
+	CsvLogger myLogger;
+	auto col1 = myLogger.addColumn("foo");
+	auto col2 = myLogger.addColumn("bar");
+	myLogger.startNewLogFile();
+	
+	myLogger.logData(CsvLogger::Cell(col1, 1));
+	myLogger.logData(vector<CsvLogger::Cell>({CsvLogger::Cell(col1, 2), CsvLogger::Cell(col2, 3)}));
+	
+	return 0;
+}
