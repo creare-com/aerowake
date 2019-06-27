aa81c44b90e17fe2e1922fa768a3dc7f0a10b2a6
John Walthour <jdw@creare.com>
2019-06-27 12:07:22 -0400

    Updated to be a loop

--- Flight source/FlightRecorder/spi_test.py
+++ Flight source/FlightRecorder/spi_test.py
@@ -8,19 +8,21 @@ for dev in test_busses:
         print("Opening /dev/spidev%d.%d"%dev)
         bus,device = dev
         spi.open(*dev)
-        outbound = [1,2,3,4]
-        inbound = spi.xfer(outbound)
-        print("spi.xfer(outbound) results in: outbound: " + repr(outbound) + ", inbound: " + repr(inbound))
-        inbound = spi.readbytes(4)
-        print("spi.readbytes(4) results in: inbound: " + repr(inbound))
-        print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
-        oldval = spi.cshigh
-        spi.cshigh = True
-        print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
-        spi.cshigh = oldval
-        print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
-        print("Closing.")
-        spi.close()
+        while True:
+            outbound = [1,2,3,4]
+            inbound = spi.xfer(outbound)
+            print("spi.xfer(outbound) results in: outbound: " + repr(outbound) + ", inbound: " + repr(inbound))
+        # inbound = spi.readbytes(4)
+        # print("spi.readbytes(4) results in: inbound: " + repr(inbound))
+        # print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
+        # oldval = spi.cshigh
+        # spi.cshigh = True
+        # print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
+        # spi.cshigh = oldval
+        # print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
         
     except Exception as e:
         print("Caught: " + repr(e))
+    finally:
+        print("Closing.")
+        spi.close()
