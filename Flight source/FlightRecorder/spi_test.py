1603492615a92692504a4a6774da6ea15909e658
John Walthour <jdw@creare.com>
2019-07-18 09:09:22 -0400

    Updates during testing

--- Flight source/FlightRecorder/spi_test.py
+++ Flight source/FlightRecorder/spi_test.py
@@ -1,6 +1,6 @@
 import spidev
 
-test_busses = [(0,0)]
+test_busses = [(0,0), (0,1)]
 
 for dev in test_busses:
     try:
@@ -9,14 +9,15 @@ for dev in test_busses:
         bus,device = dev
         spi.open(*dev)
         spi.max_speed_hz = 5000
+        print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
         # while True:
         if True:
-            outbound = [1,2,3,4]
+            # outbound = [1,2,3,4]
+            outbound = [0,0,0,0,0,0,0,0]
             inbound = spi.xfer(outbound)
             print("spi.xfer(outbound) results in: outbound: " + repr(outbound) + ", inbound: " + repr(inbound))
         # inbound = spi.readbytes(4)
         # print("spi.readbytes(4) results in: inbound: " + repr(inbound))
-        # print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
         # oldval = spi.cshigh
         # spi.cshigh = True
         # print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
