c96e0ed5ff3ec77d07ea7dec4488cf626718926b
John Walthour <jdw@creare.com>
2019-07-19 09:09:15 -0400

    TEmporarily flipped this to run the SpiSensorTest binary that was already on the pi

--- Flight source/FlightRecorder/run_flight_recorder.sh
+++ Flight source/FlightRecorder/run_flight_recorder.sh
@@ -3,6 +3,7 @@
 # /home/pi/FlightRecorder/FlightRecorder -p /dev/ttyS0
 
 while [ 0 ] ; do
-    python /home/pi/FlightRecorder/spi_test.py;
+    /home/pi/FlightRecorder/SpiSensorTest
+    # python /home/pi/FlightRecorder/spi_test.py;
     sleep 1;
 done
