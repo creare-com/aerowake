f041444e82e718ab4aaff2d6b8048e10bf878615
John Walthour <jdw@creare.com>
2019-07-25 10:29:03 -0400

    +multiplexer stimulus program

--- Flight source/FlightRecorder/run_flight_recorder.sh
+++ Flight source/FlightRecorder/run_flight_recorder.sh
@@ -5,6 +5,7 @@
 while [ 0 ] ; do
     # /home/pi/FlightRecorder/SpiSensorTest
     # python /home/pi/FlightRecorder/spi_test.py;
-    python /home/pi/FlightRecorder/spi_test_read_dlhr_status_cs0.py;
+    # python /home/pi/FlightRecorder/spi_test_read_dlhr_status_cs0.py;
+    python /home/pi/FlightRecorder/spi_test_set_mux_and_read.py;
     sleep 1;
 done
---
+++ Flight source/FlightRecorder/spi_test_set_mux_and_read.py
@@ -0,0 +1,52 @@
+"""
+Assumes a DLHR pressure sensor from AllSensors is connected to SPI0, CS0.
+
+Attempts to read the status word once.
+
+"""
+import spidev
+
+def reprHex(intArr):
+    """
+    intArr: a list of ints 0-255
+    return: string like '[0x01, 0x02, 0x03, 0x04]'
+    """
+    return '[' + (', '.join(["0x%02X"%c for c in intArr])) + ']'
+    
+def send(spiDevice, outbound):
+    """
+    spiDevice: an open spidev.SpiDev instance
+    outbound: a list of ints to transmit, 0-255.  This function may modify this variable.
+    """
+    print("Sending: " + reprHex(outbound))
+    inbound = spiDevice.xfer(outbound)
+    print("Received: " + reprHex(inbound))
+    
+
+try:
+    print("Opening mux port.")
+    muxPort = spidev.SpiDev()
+    muxPort.open(0, 0)
+    muxPort.max_speed_hz = 5000
+    print("Opening sensor port.")
+    sensorPort = spidev.SpiDev()
+    sensorPort.open(0, 1)
+    sensorPort.max_speed_hz = 5000
+    # print("spi.bits_per_word: %d, spi.cshigh: %d, spi.loop: %d, spi.no_cs: %d, spi.lsbfirst: %d, spi.max_speed_hz: %d, spi.mode: %d, spi.threewire: %d"%(spi.bits_per_word, spi.cshigh, spi.loop, spi.no_cs, spi.lsbfirst, spi.max_speed_hz, spi.mode, spi.threewire))
+    print("Setting mux to port 1 (counting from 1).")
+    send(muxPort, [0x00])
+    print("Reading DLHR status.")
+    send(sensorPort, [0xF0])
+    print("Setting mux to port 14 (counting from 1).")
+    send(muxPort, [0x0D])
+    print("Reading MAX6682 value.")
+    send(sensorPort, [0x00, 0x00])
+    print("Setting mux to port 2 (counting from 1).")
+    send(muxPort, [0x01])
+    
+except Exception as e:
+    print("Caught: " + repr(e))
+finally:
+    print("Closing.")
+    muxPort.close()
+    sensorPort.close()
