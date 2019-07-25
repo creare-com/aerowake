4593c9d3cacdf77492fed0f1f0c5f5266e10ff19
John Walthour <jdw@creare.com>
2019-07-25 09:21:42 -0400

    Switched stimulus program

--- Flight source/FlightRecorder/run_flight_recorder.sh
+++ Flight source/FlightRecorder/run_flight_recorder.sh
@@ -3,7 +3,8 @@
 # /home/pi/FlightRecorder/FlightRecorder -p /dev/ttyS0
 
 while [ 0 ] ; do
-    /home/pi/FlightRecorder/SpiSensorTest
+    # /home/pi/FlightRecorder/SpiSensorTest
     # python /home/pi/FlightRecorder/spi_test.py;
+    python /home/pi/FlightRecorder/spi_test_read_dlhr_status_cs0.py;
     sleep 1;
 done
