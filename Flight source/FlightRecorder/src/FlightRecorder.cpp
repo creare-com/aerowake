2e31d8cd4e4164dbbfca92e1942da00aebe8e75d
John Walthour <jdw@creare.com>
2019-08-01 12:25:14 -0400

    ADded boolean options to turn on and off the three main features of the logger.

--- Flight source/FlightRecorder/src/FlightRecorder.cpp
+++ Flight source/FlightRecorder/src/FlightRecorder.cpp
@@ -25,15 +25,22 @@ extern "C" void handle_signal(int sig) {
 	}
 }
 
-
+/**
+ * Return a human-readable string for a boolean
+ */
+static inline string boolToString(bool arg) {
+	return arg? "1":"0";
+}
 
 int main(int argc, char** argv)
 {
 	CLI::App cmdOpts{"Wake Swarm Flight data recorder"};
 	
 	// Command line options
-	string extension = "png";
-	cmdOpts.add_option("-x", extension, "File format, as an extension, such as \"bmp\" or \"png\" (omit the quotes).  Default is \"" + extension + "\".  Must be supported by OpenCV's imwrite(). \"bmp\" is fast (10ms), \"png\" is compact.");
+	bool recImages = true;
+	cmdOpts.add_option("--images", recImages, "1 to enable, 0 to disable reading and recording images from the camera. Default is " + boolToString(recImages) + ".");
+	string imageExtension = "png";
+	cmdOpts.add_option("-x", imageExtension, "Camera image file format, as an extension, such as \"bmp\" or \"png\" (omit the quotes).  Default is \"" + imageExtension + "\".  Must be supported by OpenCV's imwrite(). \"bmp\" is fast (10ms), \"png\" is compact.");
 	string dirFormat = "./%F_%H-%M-%S";
 	// WARNING: You can inject bash commands here.  For example, try: -d "'; echo hello; '"
 	cmdOpts.add_option("-d", dirFormat, "Pattern specifying the directory at which to save the recording. Default: \"" + dirFormat + "\".  Will substitute flags found here: https://howardhinnant.github.io/date/date.html#to_stream_formatting.  '/' characters are directory separators.  If no leading '/', will be relative to working directory.  Avoid any characters not supported by the filesystem, such as colons.");
@@ -42,11 +49,19 @@ int main(int argc, char** argv)
 	string cameraSettingsPath = "camera_settings.csv";
 	cmdOpts.add_option("-c", cameraSettingsPath, "Path to camera settings file.  Default is " + cameraSettingsPath + ". Must follow a very specific CSV format - see example file.");
 	string apLogFilenameFormat = "ap_%F_%H-%M-%S.csv";
+
+	bool recAp = true;
+	cmdOpts.add_option("--ap", recAp, "1 to enable, 0 to disable connecting to and recording data from the autopilot.  Default is " + boolToString(recAp) + ".");
+	bool autostart = false;
+	cmdOpts.add_option("--autostart", autostart, "Wait for the autopilot to arm, then begin recording.  Use with --ap 1.  Default is " + boolToString(autostart) + ".");
 	cmdOpts.add_option("-a", apLogFilenameFormat, "Pattern specifying the filename for data captured from the autopilot. Default: \"" + apLogFilenameFormat + "\".  Will substitute flags found here: https://howardhinnant.github.io/date/date.html#to_stream_formatting.  Extension should be included here.  Avoid any characters not supported by the filesystem, such as colons.");
 	string autopilotPort = "/dev/ttyTHS1"; // UART0 on the Jetson TX2 with Auvidea J120's DTS
 	cmdOpts.add_option("-p", autopilotPort, "Port to which the Pixhawk Autopilot is connected.  Default is " + autopilotPort + ".");
 	int apBaudRate = 57600;
 	cmdOpts.add_option("-r", apBaudRate, "Baud rate for communicating with the Pixhawk Autopilot.  Default is 57600.");
+
+	bool recProbe = true;
+	cmdOpts.add_option("--pressure", recProbe, "1 to enable, 0 to disable reading and recording pressures from the wind probe.  Default is " + boolToString(recProbe) + ".");
 	string wpLogFilenameFormat = "wp_%F_%H-%M-%S.csv";
 	cmdOpts.add_option("-w", wpLogFilenameFormat, "Pattern specifying the filename for data captured from the wind probe. Default: \"" + apLogFilenameFormat + "\".  Will substitute flags found here: https://howardhinnant.github.io/date/date.html#to_stream_formatting.  Extension should be included here.  Avoid any characters not supported by the filesystem, such as colons.");
 	CLI11_PARSE(cmdOpts, argc, argv); // This will exit if the user said "-h" or "--help"
@@ -54,8 +69,17 @@ int main(int argc, char** argv)
 	int result = 0;
 	signal(SIGINT, handle_signal);
 	
+	// Set up benchmarking
+	list<const Benchmarker *> allBms;
+	Benchmarker bmWholeFrame("Entire frame");
+	Benchmarker bmSync("Sync filesystem");
+	allBms.push_back(&bmWholeFrame);
+	allBms.push_back(&bmSync);
+
 	// Print application build information
 	cout << "Application build date: " << __DATE__ << " " << __TIME__ << endl << endl;
+
+	// cout << "recImages: " << recImages <<", recAp: " << recAp << ", recProbe: " << recProbe <<", autostart: " << autostart << endl;
 
 	// Create directory to store recording
 	string recordingDir = date::format(dirFormat, date::floor<milliseconds>(system_clock::now()));
@@ -63,37 +87,32 @@ int main(int argc, char** argv)
 	// Single quotes make it slightly harder to route a bash command in this way
 	system(("mkdir -p '" + recordingDir + "'").c_str());
 	
-	// Start logging data from the autopilot in another thread
-	// AutopilotLogger apLogger(recordingDir, apLogFilenameFormat, autopilotPort, apBaudRate);
-	// apLogger.startLogging();
+	// Setup autopilot logger
+	AutopilotLogger apLogger(recordingDir, apLogFilenameFormat, autopilotPort, apBaudRate);
+	if(recAp) {
+		// Start logging data from the autopilot in another thread
+		apLogger.startLogging();
+	}
 	
 	// Setup wind probe logger
-	// WindProbeLogger wpLogger(recordingDir, apLogFilenameFormat, autopilotPort, apBaudRate);
+	WindProbeLogger wpLogger(recordingDir, apLogFilenameFormat, autopilotPort, apBaudRate);
 	
-	list<const Benchmarker *> allBms;
-	Benchmarker bmWholeFrame("Entire frame");
-	Benchmarker bmSync("Sync filesystem");
-	allBms.push_back(&bmWholeFrame);
-	allBms.push_back(&bmSync);
-
-	CameraLogger camLogger(recordingDir, imageFilenameFormat, extension, allBms);
-	camLogger.initCamera(cameraSettingsPath);
+	// Set up camera logger
+	CameraLogger camLogger(recordingDir, imageFilenameFormat, imageExtension, allBms);
+	if(recImages) {
+		camLogger.initCamera(cameraSettingsPath);
+	}
 	try {
 		while(caught_exit == 0) {
 			bmWholeFrame.start();
 			
-			camLogger.captureAndLogImage();
+			// This is the thread performing image logging
+			if(recImages) { camLogger.captureAndLogImage(); }
 			
 			bmSync.start();
 			sync();
 			bmSync.end();
 			
-			// cout << "Sending heartbeat message" << endl;
-			// mavlink_heartbeat_t heartbeat;
-			// mavlink_msg_heartbeat_encode(/*uint8_t system_id*/ 1, /*uint8_t component_id*/0, &message, &heartbeat);
-			// apSerialPort.write_message(message);
-			// cout << "Sent." << endl;
-
 			bmWholeFrame.end();
 		}
 		cout << "Exiting." << endl;
@@ -101,7 +120,7 @@ int main(int argc, char** argv)
 		cout << "Error in main loop: " << e.what() << endl;
 	}
 
-	// apLogger.stopLogging();
+	apLogger.stopLogging();
 
 	sync();
 	Benchmarker::summarizeBenchmarksToStream(allBms, cout);
