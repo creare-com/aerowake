b067d56b1a118bd40d13e2085a5bc713f075e98e
John Walthour <jdw@creare.com>
2020-01-02 15:12:51 -0500

    turned off bootup activity

--- Flight source/FlightRecorder/run_flight_recorder.sh
+++ Flight source/FlightRecorder/run_flight_recorder.sh
@@ -2,10 +2,10 @@
 
 # /home/pi/FlightRecorder/FlightRecorder -p /dev/ttyS0
 
-while [ 0 ] ; do
+# while [ 0 ] ; do
     # /home/pi/FlightRecorder/SpiSensorTest
     # python /home/pi/FlightRecorder/spi_test.py;
     # python /home/pi/FlightRecorder/spi_test_read_dlhr_status_cs0.py;
-    python /home/pi/FlightRecorder/spi_test_set_mux_and_read.py;
-    sleep 1;
-done
+    # python /home/pi/FlightRecorder/spi_test_set_mux_and_read.py;
+    # sleep 1;
+# done
