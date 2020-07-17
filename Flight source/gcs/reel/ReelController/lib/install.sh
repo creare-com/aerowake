4c85226bc568fb70be61d31b1ecf714c3c838183
John Walthour <jdw@creare.com>
2020-07-17 10:07:10 -0400

    Fixed paths in installer script

--- Flight source/gcs/reel/ReelController/lib/install.sh
+++ Flight source/gcs/reel/ReelController/lib/install.sh
@@ -2,11 +2,15 @@
 sudo cp 99-ftdi.rules /etc/udev/rules.d/
 # sudo cp libEposCmd.so.5.0.1.0 /usr/lib
 # sudo cp libftd2xx.so.1.2.8 /usr/lib
-# cd /usr/lib
-sudo ln -s libEposCmd.so.5.0.1.0 libEposCmd.so
-sudo ln -s libftd2xx.so.1.2.8 libftd2xx.so
+CALLER_PATH=`pwd`
+cd "`dirname "$0"`"
+MY_PATH=`pwd`
+cd "$CALLER_PATH"
+echo "My path is $MY_PATH, being called from $CALLER_PATH"
+sudo ln -s "$MY_PATH/libEposCmd.so.5.0.1.0" /usr/lib/libEposCmd.so
+sudo ln -s "$MY_PATH/libftd2xx.so.1.2.8" /usr/lib/libftd2xx.so
 
 sudo service udev restart
 
-sudo pip install cython Adafruit_ADS1X15 enum
+sudo pip3 install cython Adafruit_ADS1X15
 
